a41db24729823d5bdb4e1b5eaedfe7cfc47dcd25
Trevor Storey <trevor@photoshelter.com>
2019-11-24 16:14:04 -0700

    [template-strings] finish exercises

--- template-strings/ex.fixed.js
+++ template-strings/ex.fixed.js
@@ -14,6 +14,5 @@ var name = "kyle",
 	topic = "JS Recent Parts";
 
 console.log(
-	upper`Hello ${name} (@${twitter}), welcome to ${topic}!` ===
-	"Hello KYLE (@GETIFY), welcome to JS RECENT PARTS!"
+	upper`Hello ${name} (@${twitter}), welcome to ${topic}!`
 );
