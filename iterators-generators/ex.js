f93be3007c131417324426aff4fc69414cf8b8e7
Trevor Storey <trevor@photoshelter.com>
2019-11-24 19:07:07 -0700

    [iterators-generators] implementation exercise

--- iterators-generators/ex.js
+++ iterators-generators/ex.js
@@ -1,14 +1,22 @@
 var numbers = {
-	// ..
+	*[Symbol.iterator]({
+    start = 0,
+    stop = 100,
+    step = 1
+  } = {}) {
+    for (let num = start; num <= stop; num += step) {
+      yield num;
+    }
+  }
 };
 
 // should print 0..100 by 1s
-for (let num of numbers) {
-	console.log(num);
-}
+// for (let num of numbers) {
+// 	console.log(num);
+// }
 
 // should print 6..30 by 4s
-console.log("My lucky numbers are: ____");
+console.log(`My lucky numbers are: ${[...numbers[Symbol.iterator]({ start: 6, stop: 30, step: 4 })]}`);
 
 // Hint:
 //     [...numbers[Symbol.iterator]( ?? )]
