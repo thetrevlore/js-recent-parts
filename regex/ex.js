d8bcb042c8ff63c768d2b1171d59f2709631c44a
Trevor Storey <trevor@photoshelter.com>
2019-11-24 19:26:23 -0700

    [regex] implement exercise

--- regex/ex.js
+++ regex/ex.js
@@ -23,9 +23,13 @@ for (let power of powers(poem)) {
 
 // Hints:
 //
-// function *powers(poem) { .. }
-//
-// re = / .. /gs
-//
-// while (match = re.exec(poem)) { .. }
+function *powers(poem) {
+  var re = /(?<=power of )(?<thing>(a )?\w+).*?(?<=can )(?<verb>\w+)/gs;
+  var match;
+	while (match = re.exec(poem)) {
+		let { groups: { thing, verb } } = match;
+		yield `${thing}: ${verb}`;
+	}
+}
+
 //
